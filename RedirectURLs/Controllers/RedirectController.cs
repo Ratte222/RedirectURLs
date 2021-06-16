523ba6eab57f6841c917af68f62d6f7af3c53170
Ratte222 <Artur21111999.o@gmail.com>
2021-06-16 14:33:07 +0300

    Добавьте файлы проекта.

---
+++ RedirectURLs/Controllers/RedirectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedirectURLs.Controllers
+{
+    public class RedirectController : Controller
+    {
+        public IActionResult Index(string shortURL)
+        {
+
+            return Redirect("https://www.youtube.com/c/%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%8B%D1%81%D0%BB%D0%B8%D0%92%D0%B8%D0%B4%D0%B5%D0%BE%D1%83%D1%80%D0%BE%D0%BA%D0%B8/videos");
+        }
+    }
+}
---
+++ RedirectURLs/Crypt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedirectURLs
+{
+    public class Crypt
+    {
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value">сгенерировать хеш для этого значения</param>
+        /// <returns>hash for message in Base64String</returns>
+        public static string GetHashSHA512(string value)
+        {
+            byte[] val = Encoding.Default.GetBytes(value);
+            return Convert.ToBase64String(InternalGetHashSHA512(val));
+
+        }
+        private static byte[] InternalGetHashSHA512(byte[] value)
+        {
+            byte[] hashValue = new byte[512];
+            using (SHA512 mySHA512 = SHA512.Create())
+            {
+                hashValue = mySHA512.ComputeHash(value);
+            }
+            return hashValue;
+        }
+    }
+}
